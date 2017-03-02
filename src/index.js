352bd4a0ea8de27b7886a4312eb5c60078d5741f
PakkuDon <pakku.pd@gmail.com>
2017-03-02 16:19:35 +1100

    Remove React

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 import 'normalize.css/normalize.css';
 import './stylesheets/app.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <h1>Hello</h1>,
-  document.querySelector('#root')
-);
