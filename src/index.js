4f4142347ead07b1a93177a3a9b04595f19f47e2
PakkuDon <pakku.pd@gmail.com>
2017-03-05 23:59:19 +1100

    Add contact info

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import 'normalize.css/normalize.css';
 import './stylesheets/app.scss';
+
+function initMap(mapElem) {
+  var officeLocation = {
+    lat: -37.815341,
+    lng: 144.958493
+  };
+  var map = new google.maps.Map(mapElem, {
+    zoom: 15,
+    center: officeLocation
+  });
+  var marker = new google.maps.Marker({
+    position: officeLocation,
+    map: map,
+    title: 'Teamsquare'
+  });
+}
+
+initMap(document.querySelector('#map'));
